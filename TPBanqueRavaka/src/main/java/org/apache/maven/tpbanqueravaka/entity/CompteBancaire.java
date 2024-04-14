219417df35b42b0b845af1f060b3bbc8d531f86c
Ravaka82 <89504880+Ravaka82@users.noreply.github.com>
2024-04-14 16:31:50 +0300

    Écriture d'une entité CompteBancaire

---
+++ TPBanqueRavaka/src/main/java/org/apache/maven/tpbanqueravaka/entity/CompteBancaire.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.apache.maven.tpbanqueravaka.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.io.Serializable;
+
+/**
+ *
+ * @author user
+ */
+@Entity
+public class CompteBancaire implements Serializable {
+
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String nomProprietaire;
+    private int solde;
+
+    public CompteBancaire() {
+    }
+
+    public CompteBancaire(String nomProprietaire, int solde) {
+        this.nomProprietaire = nomProprietaire;
+        this.solde = solde;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomProprietaire() {
+        return nomProprietaire;
+    }
+
+    public void setNomProprietaire(String nomProprietaire) {
+        this.nomProprietaire = nomProprietaire;
+    }
+
+    public int getSolde() {
+        return solde;
+    }
+
+    public void setSolde(int solde) {
+        this.solde = solde;
+    }
+
+    public void deposer(int montant) {
+        solde += montant;
+    }
+
+    public void retirer(int montant) {
+        if (montant < solde) {
+            solde -= montant;
+        } else {
+            solde = 0;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof CompteBancaire)) {
+            return false;
+        }
+        CompteBancaire other = (CompteBancaire) object;
+        return !((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id)));
+    }
+
+    @Override
+    public String toString() {
+        return "CompteBancaire[ id=" + id + ", nomProprietaire=" + nomProprietaire + ", solde=" + solde + " ]";
+    }
+}
